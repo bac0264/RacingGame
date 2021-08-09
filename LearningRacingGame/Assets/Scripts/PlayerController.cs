9c259a2ec397c544fd41618fcf53cbefa76484f8
bacnt <bacnt97@gmail.com>
2021-08-09 17:52:26 +0700

    Add Lession 5

--- LearningRacingGame/Assets/Scripts/PlayerController.cs
+++ LearningRacingGame/Assets/Scripts/PlayerController.cs
@@ -4,21 +4,35 @@ public class PlayerController : MonoBehaviour
 {
     // Tốc độ của xe
 
-    private float speed;
-    
+    public float speed;
+    public float turnSpeed;
+    public float horizontalInput;
+
+    public float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
         // Start dùng để khởi tạo trước lúc vào game
 
         speed = 20;
+        turnSpeed = 15;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Cập nhật run-time trong game.
-
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        
+        // Lấy thông số khi nhấn vào nút trái/phải
+        horizontalInput = Input.GetAxis("Horizontal");
+        
+        // Lấy thông số khi nhấn vào nút tiến/lùi
+        forwardInput = Input.GetAxis("Vertical");
+        
+        // Di chuyển xe dựa vào forward input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       
+        // Xoay xe dựa vào horizon input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }
