549cb71efb945f0f02ba4353ed8eb0197af49178
bacnt <bacnt97@gmail.com>
2021-08-09 16:58:15 +0700

    Add Lession 3

---
+++ LearningRacingGame/Assets/Scripts/CameraFollowVehicle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class CameraFollowVehicle : MonoBehaviour
+{
+    public GameObject vehicle;
+
+    private Vector3 offset = new Vector3(0, 5, -15);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Smooth the Camera with LateUpdate
+
+    private void LateUpdate()
+    {
+        transform.position = vehicle.transform.position + offset;
+    }
+}
--- LearningRacingGame/Assets/Scripts/PlayerController.cs
+++ LearningRacingGame/Assets/Scripts/PlayerController.cs
@@ -2,19 +2,23 @@
 
 public class PlayerController : MonoBehaviour
 {
+    // Tốc độ của xe
+
+    private float speed;
+    
     // Start is called before the first frame update
     void Start()
     {
         // Start dùng để khởi tạo trước lúc vào game
-        Debug.Log("Start");
+
+        speed = 20;
     }
 
     // Update is called once per frame
     void Update()
     {
         // Cập nhật run-time trong game.
-        Debug.Log("Update");
-        
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
     }
 }
